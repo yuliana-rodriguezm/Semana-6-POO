7b68d1097963f52337b2b0d93df31356c5098592
yuliana-rodriguezm <ye.rodriguezm@uea.edu.ec>
2026-01-17 15:40:16 -0500

    Implementación de main, y comprobación de que el código funciona

--- modelo/maquillaje.py
+++ modelo/maquillaje.py
@@ -1,19 +1,23 @@
-# Clase derivada Cosmetico
-# Aquí aplicamos HERENCIA y POLIMORFISMO
+from modelo.producto import Producto
 
-from Modelo.producto import Producto
+# Clase hija Maquillaje
+# Aquí se aplica:
+# - HERENCIA
+# - POLIMORFISMO
 
-
-class Cosmetico(Producto):
+class Maquillaje(Producto):
     def __init__(self, nombre: str, precio: float, tipo_piel: str):
-        # Llamamos al constructor de la clase base (herencia)
+        
+        # Llamamos al constructor de la clase base (HERENCIA)
         super().__init__(nombre, precio)
         self.tipo_piel = tipo_piel
 
     # Sobrescribimos el método (POLIMORFISMO)
     def mostrar_info(self) -> str:
         return (
-            f"Cosmético: {self._nombre} | "
+            f"Maquillaje: {self._nombre} | "
             f"Precio: ${self._precio} | "
             f"Tipo de piel: {self.tipo_piel}"
         )
+
+
